1b3e8883d0e090599cce25e6d41109f338ba0c98
Abhisek <z2apodcast@gmail.com>
2024-01-19 14:22:06 +0530

    updates setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+def get_requirements(filepath: str) -> List[str]:
+    requirements = []
+
+    with open(filepath) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [i.replace("\n", "") for i in requirements]
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+setup(
+    name="ML_Pipeline_project",
+    version="0.0.1",
+    description = "Machine Learning Pipeline Project",
+    author = "Abhisek Bag",
+    author_email = "z2apodcast@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt")
+)
